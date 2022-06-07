d2b53586d08209b9b0f19c0a68e6b801eed13c29
pistamarcel <marcel.pista96@e-uvt.ro>
2022-06-07 21:56:55 +0300

    tema 2.2 primul commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Cerintele trebuie mutate in main pentru a functiona!");
     }
 }
